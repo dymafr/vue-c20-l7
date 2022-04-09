5fc6acbf0993eae7a0ee37bceb7a5e1347488815
dymafr <erwan1309@hotmail.com>
2022-04-09 10:47:03 +0200

    1 file added

---
+++ src/shared/services/todo.service.ts
@@ -0,0 +1,25 @@
+import { Todo } from '../interfaces/todo.interface';
+
+const BASE_URL = 'https://restapi.fr/api/vue3todos';
+
+export async function fetchTodo(): Promise<Todo[]> {
+  return await (await fetch(BASE_URL)).json();
+}
+
+export async function deleteTodo(todoId: string): Promise<string> {
+  return await (
+    await fetch(`${BASE_URL}/${todoId}`, { method: 'DELETE' })
+  ).json();
+}
+
+export async function updateTodo(
+  todoId: string,
+  update: Partial<Todo>
+): Promise<Todo> {
+  return await (
+    await fetch(`${BASE_URL}/${todoId}`, {
+      method: 'PATCH',
+      body: JSON.stringify(update),
+    })
+  ).json();
+}
